d9687bd05a600ca6d05617bb5ef6b5dfa90152c9
SOWON <48439367+devSOWON0628@users.noreply.github.com>
2025-12-01 20:05:09 +0900

    fix: logger 수정

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.useLogger(app.get(WINSTON_MODULE_PROVIDER));
 
   app.useGlobalPipes(
     new ValidationPipe({
